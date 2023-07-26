cbbf80533511e3aec1bc52e2d7d1696f836b4130
Rares Cruceat <cruceat.rares@gmail.com>
2023-07-26 16:22:40 +0300

    add all required libraries in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.google.firebase.crashlytics")
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -54,24 +55,46 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    val roomVersion = "2.5.2"
+    val coreKtx = "1.10.1"
+    val kotlinCoroutines = "1.6.4"
+    val lifecycleRuntimeKtx = "2.6.1"
+    val activityCompose = "1.7.2"
+    val composeBom = "2023.03.00"
+    val firebaseBom = "32.2.0"
+    val hilt = "2.44"
+    val retrofit = "2.9.0"
+    val moshi = "1.14.0"
+
+    val jUnit = "4.13.2"
+    val extJUnit = "1.1.5"
+    val espressoCore = "3.5.1"
+
+
+    implementation("androidx.core:core-ktx:$coreKtx")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntimeKtx")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinCoroutines")
+    implementation("androidx.activity:activity-compose:$activityCompose")
+    implementation(platform("androidx.compose:compose-bom:$composeBom"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
+    implementation(platform("com.google.firebase:firebase-bom:$firebaseBom"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:$hilt")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt")
+    implementation("com.squareup.retrofit2:retrofit:$retrofit")
+    implementation("com.squareup.moshi:moshi:$moshi")
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    testImplementation("junit:junit:$jUnit")
+    androidTestImplementation("androidx.test.ext:junit:$extJUnit")
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoCore")
+    androidTestImplementation(platform("androidx.compose:compose-bom:$composeBom"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("com.google.gms.google-services") version "4.3.15" apply false
     id("com.google.firebase.crashlytics") version "2.9.7" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
